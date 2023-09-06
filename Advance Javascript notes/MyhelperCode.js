4d45de2451e686bfa53db2802a74815c2c962150
adarshnaikdev <nqvadi@gmail.com>
2023-09-07 00:49:58 +0530

    gfg problems

---
+++ Advance Javascript notes/MyhelperCode.js
@@ -0,0 +1,24 @@
+let ranNum = Math.floor(Math.random()*5)
+
+console.log(ranNum);
+
+if(ranNum ==0)
+{
+    console.log("Revise Deepa chourasi's js advance notes once again");
+}
+if(ranNum ==1)
+{
+    console.log("Revise questpond JS concepts along with closure");
+}
+if(ranNum ==2)
+{
+    console.log("Solve 2 JS problems from gfg")
+}
+if(ranNum ==3)
+{
+    console.log("Learn the remaining concepts like generator functions and function currying ");
+}
+if(ranNum ==4)
+{
+    console.log("watch and try hooks in react")
+}
