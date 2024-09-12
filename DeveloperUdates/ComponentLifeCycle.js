9641930f63c237cc488e7ba0798816a7f43b65b2
adarshnaikdev <nqvadi@gmail.com>
2024-09-13 02:17:31 +0530

    infocus interview questions

---
+++ DeveloperUdates/ComponentLifeCycle.js
@@ -0,0 +1,19 @@
+import React,{useEffect} from 'react'
+
+function ComponentLifeCycle() {
+
+    useEffect(()=>{
+        //operstion
+
+        return {
+            //clean up function
+        }
+    },[variable])
+ 
+
+  return (
+    <div>ComponentLifeCycle</div>
+  )
+}
+
+export default ComponentLifeCycle
